07e55c5f0d37b9458ccd28063d38bf629e7f7ce2
daniel81021 <99546201+daniel81021@users.noreply.github.com>
2023-06-21 11:33:44 +0100

    Task DEMO-21: test corrected

--- src/test/java/com/example/BookApp/Publisher/repository/PublisherJpaRepositoryTest.java
+++ src/test/java/com/example/BookApp/Publisher/repository/PublisherJpaRepositoryTest.java
@@ -1,28 +1,36 @@
-package com.example.BookApp.publisher.repository;
+package com.example.BookApp.Publisher.repository;
 
 import com.example.BookApp.publisher.domain.PublisherJpa;
+import com.example.BookApp.publisher.repository.PublisherJpaRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.stream.Stream;
+
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class PublisherJpaRepositoryTest {
 
-    private final String NAME = "Publisher";
+    private static final String NAME = "Publisher";
+    private static final String NAME_2 = "Publisher 2";
 
 
     @Autowired
     private PublisherJpaRepository publisherJpaRepository;
 
-    @Test
-    void save(){
+    @ParameterizedTest
+    @MethodSource("provideNames")
+    void save(String input, String result) {
 
         // given
-        PublisherJpa publisherJpa = createPublisherJpa(NAME);
+        PublisherJpa publisherJpa = createPublisherJpa(input);
 
         // when
         PublisherJpa saved = publisherJpaRepository.save(publisherJpa);
@@ -34,28 +42,35 @@ class PublisherJpaRepositoryTest {
         Assertions.assertThat(saved.getAudit().getCreatedOn()).isNotNull();
         Assertions.assertThat(saved.getAudit().getUpdatedOn()).isNotNull();
         Assertions.assertThat(saved.getAudit().getUpdatedOn()).isEqualTo(saved.getAudit().getCreatedOn());
-        Assertions.assertThat(saved.getName()).isEqualTo(NAME);
+        Assertions.assertThat(saved.getName()).isEqualTo(result);
     }
 
     @Test
-    void shouldThrowExceptionWhenPublisherExists(){
+    void shouldThrowExceptionWhenPublisherExists() {
 
         // given
         PublisherJpa publisherJpa = createPublisherJpa(NAME);
 
-        PublisherJpa saved = publisherJpaRepository.save(publisherJpa);
+        publisherJpaRepository.save(publisherJpa);
 
         PublisherJpa publisherJpa2 = createPublisherJpa(NAME);
 
         // when
         // then
-        Assertions.assertThatThrownBy(()-> publisherJpaRepository.save(publisherJpa2))
+        Assertions.assertThatThrownBy(() -> publisherJpaRepository.save(publisherJpa2))
                 .isInstanceOf(DataIntegrityViolationException.class);
     }
-    private PublisherJpa createPublisherJpa(String name){
+
+    private PublisherJpa createPublisherJpa(String name) {
         PublisherJpa publisherJpa = new PublisherJpa();
         publisherJpa.setName(name);
         return publisherJpa;
     }
 
+    private static Stream<Arguments> provideNames() {
+        return Stream.of(
+                Arguments.of(NAME, NAME),
+                Arguments.of(NAME_2, NAME_2)
+        );
+    }
 }
